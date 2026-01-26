3152a51a7a837479252c493d1569de024fb7b096
unitf40gmail.com <unitf40@gmail.com>
2026-01-26 20:38:14 +0200

    \\

--- EGE/ege 8.py
+++ EGE/ege 8.py
@@ -148,3 +148,115 @@
 # B = set(b)
 # res = A ^ B
 # print(len(res))
+
+
+# from itertools import *
+# k=0
+# for i in product("АБВГЭЮЯ",repeat=5):
+#     s="".join(i)
+#     if (s[0] in "ЭЮЯ") and (s[-1] in "ЭЮЯ") and all(c not in "ЭЮЯ" for c in s[1:4]):
+#         k+=1
+# print(k)
+
+# from itertools import *
+# k=0
+# for i in product("АБВГДЯ", repeat=5):
+#     s=''.join(i)
+#     if s.count("Я")==3:
+#         k+=1
+# print(k)
+
+# from itertools import *
+# k=0
+# for i in product("KATER",repeat=3):
+#     s="".join(i)
+#     if s.count("R")>=2:
+#         k+=1
+# print(k)
+
+# from itertools import *
+# k=0
+# for i in product("ДЖОБС",repeat=6):
+#     s="".join(i)
+#     if s.count("Д")==1 and s.count("Ж")<=2 and s.count("О")==1 and s.count("С")==1:
+#         k+=1
+# print(k)
+
+# from itertools import *
+# k=0
+# for i in product("0123456",repeat=5):
+#     s="".join(i)
+#     if (s[0] in "246") and (s[-1] not in "012") and (s.count("4")<=1):
+#         k+=1
+# print(k)
+
+# from itertools import *
+# k=0
+# for i in product("01234567", repeat=5):
+#     s="".join(i)
+#     s=s.replace("1","Q").replace("3","Q").replace("5","Q").replace("7","Q")
+#     if s.count("6")==1 and ("Q6" not in s) and ("6Q" not in s) and s[0] not in "0":
+#         k+=1
+# print(k)
+
+# from itertools import *
+# k=0
+# for i in product("0123456",repeat=6):
+#     s="".join(i)
+#     s=s.replace("0","Q").replace("2","Q").replace("4","Q").replace("6","Q")
+#     s=s.replace("1","R").replace("3","R").replace("5","R")
+#     if "".join(i)[0]!="0" and "".join(i).count("6")==1:
+#         if ("QRQRQR" in s ) or ("RQRQRQ" in s):
+#             k+=1
+# print(k)
+
+# from itertools import *
+# k=0
+# for i in product("АВОРТ",repeat=6):
+#     s="".join(i)
+#     k+=1
+#     if s=="ВОРОТА":
+#         print(k)
+
+# from itertools import *
+# k=0
+# for i in product("АЛПЦЯ",repeat=5):
+#     s="".join(i)
+#     k+=1
+#     if s.count("А")<=1 and s.count("Ц")==2 and s.count("Л")==0:
+#         print(k)
+#         break
+
+# from itertools import *
+# k=0
+# for i in product("АГИЛМОРТ",repeat=5):
+#     s="".join(i)
+#     k+=1
+#     if k%2==0 and s[0] not in "АГ" and s.count("Р")>=2:
+#         print(k)
+#         break
+
+# from itertools import *
+# k=0
+# for i in product("ABCD",repeat=4):
+#     s="".join(i)
+#     if s[0]<=s[1]<=s[2]<=s[3]:
+#         k+=1
+# print(k)
+
+# from itertools import *
+# k=0
+# for ln in range(4,7):
+#     for x in product("ЧОАНИМЕ",repeat=ln):
+#         s="".join(x)
+#         if s.count("М")==3:
+#             k+=1
+# print(k)
+
+# from itertools import *
+# k=0
+# for i in product("012345678",repeat=7):
+#     s="".join(i)
+#     if s[0] not in "037" and s[0]!=s[1] and s[1]!=s[2] and s[2]!=s[3] and s[3]!=s[4] and s[4]!=s[5] and s[5]!=s[6]:
+#         k+=1
+# print(k)
