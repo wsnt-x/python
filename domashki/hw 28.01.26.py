cfeca6f7342f5f8cad5cd60a1fcb845374b8c13c
unitf40gmail.com <unitf40@gmail.com>
2026-01-31 11:07:40 +0200

    \\

--- domashki/hw 28.01.26.py
+++ domashki/hw 28.01.26.py
@@ -5,8 +5,26 @@
 # переданное в параметре delay, пока возвращаемое значение не будет True,
 # при этом количество перезапусков не должно превышать значения, переданного
 # в параметре max_retries.
-import time
-def retry(max_retries, delay):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
+# from time import sleep
+# from random import choice
+# def retry(max_retries, delay):
+#     def decorator(func):
+#         def wrapper(*args, **kwargs):
+#             for _ in range(max_retries):
+#                 if func():
+#                     return True
+#                 sleep(delay)
+#             return False
+#         return wrapper
+#     return decorator
+#
+# @retry(max_retries=3, delay=3)
+# def check():
+#     print("Пробую...")
+#     return choice([True, False])
+# result = check()
+# print("Результат:",result)
+
+
+
 
