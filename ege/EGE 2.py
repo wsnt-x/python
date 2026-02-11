7bd4cf6beb265c4043036baadc62763045b52088
unitf40gmail.com <unitf40@gmail.com>
2026-02-11 20:50:48 +0200

    \\

--- ege/EGE 2.py
+++ ege/EGE 2.py
@@ -88,27 +88,15 @@
 
 # from itertools import *
 # def f(x1,x2,x3,x4,x5):
-#     return (x1 or (not x2) or (not x3) or x4 or (not x5)) and ((not x1) or (not x2) or x3 or x4 or x5) and (x1 or (not x2) or (not x3) or (not x4) or x5)
-# for a,b,c,d,e in product([0,1],repeat=5):
-#     table=((0,1,1,0,a,),
-#             (0,1,1,1,0),
-#            (0,1,c,d,1),
-#            (0,0,0,1,0))
-#     if len(table)==len(set(table)):
-#         for p in permutations("x1x2x3x4x5"):
-#             if [f(**dict(zip(p,r))) for r in table]==[1,b,0,e]:
-#                 print(*p)
-#
-# from itertools import *
-#
-# def f(x1,x2,x3,x4,x5):
 #     return (x1 or (not x2) or (not x3) or x4 or (not x5)) and \
 #            ((not x1) or (not x2) or x3 or x4 or x5) and \
 #            (x1 or (not x2) or (not x3) or (not x4) or x5)
-#
 # for a,b,c,d,e in product([0,1],repeat=5):
-#     t = [(0,1,1,0,a),(0,1,1,1,0),(0,1,c,d,1),(0,0,0,1,0)]
-#     if len(t)==len(set(t)):
+#     table=[(0,1,1,0,a),
+#            (0,1,1,1,0),
+#            (0,1,c,d,1),
+#            (0,0,0,1,0)]
+#     if len(table)==len(set(table)):
 #         p = ['x1','x2','x3','x4','x5']
-#         if [f(**dict(zip(p,r))) for r in t] == [1,b,0,e]:
+#         if [f(**dict(zip(p,r))) for r in table] == [1,b,0,e]:
 #             print(a,b,c,d,e)
